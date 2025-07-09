1edabe156b0422e4f09e6317feceecab181b359b
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-07-09 12:33:14 +0200

    delete unnecessary imports

--- app/src/main/java/org/codeforegypt/caffienedesign/presentation/screens/orderSizeDetailsScreen/OrderDetailsScreen.kt
+++ app/src/main/java/org/codeforegypt/caffienedesign/presentation/screens/orderSizeDetailsScreen/OrderDetailsScreen.kt
@@ -1,42 +1,28 @@
 package org.codeforegypt.caffienedesign.presentation.screens.orderSizeDetailsScreen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import org.codeforegypt.caffienedesign.presentation.screens.orderSizeDetailsScreen.OrderDetailsUiState.SeedsSize
 import org.codeforegypt.caffienedesign.R
-import org.codeforegypt.caffienedesign.presentation.components.CoffeeBeanSizeSelector
 import org.codeforegypt.caffienedesign.presentation.components.AnimatedCupDisplay
-import org.codeforegypt.caffienedesign.presentation.components.CupSize
 import org.codeforegypt.caffienedesign.presentation.components.ButtonsSize
+import org.codeforegypt.caffienedesign.presentation.components.CoffeeBeanSizeSelector
+import org.codeforegypt.caffienedesign.presentation.components.CupSize
 import org.codeforegypt.caffienedesign.presentation.components.CustomCancelButton
 import org.codeforegypt.caffienedesign.presentation.components.SizeSelectorHeader
 import org.codeforegypt.caffienedesign.presentation.navigation.Screen
-import org.codeforegypt.caffienedesign.presentation.ui.theme.Urbanist
+import org.codeforegypt.caffienedesign.presentation.screens.orderSizeDetailsScreen.OrderDetailsUiState.SeedsSize
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
